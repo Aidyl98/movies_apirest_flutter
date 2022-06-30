6acd36e0ae0bee2730868171f516dd1424a09ef8
Aidyl <jgarciaalbalah@gmail.com>
2022-06-30 00:27:52 -0400

    MoviesEvent: + FetchEvent

--- lib/bloc/movies_event.dart
+++ lib/bloc/movies_event.dart
@@ -8,3 +8,19 @@ abstract class MoviesEvent {
 class BillboardFetchEvent extends MoviesEvent {
   const BillboardFetchEvent();
 }
+
+class PopularMoviesFetchEvent extends MoviesEvent {
+  const PopularMoviesFetchEvent();
+}
+
+class ChildMoviesFetchEvent extends MoviesEvent {
+  const ChildMoviesFetchEvent();
+}
+
+class SearchMoviesFetchEvent extends MoviesEvent {
+  String textSearch;
+
+  SearchMoviesFetchEvent(
+    this.textSearch,
+  );
+}
